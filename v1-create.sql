e5887c2c0be817bc6765c38769ce447a3d43f81a
maralina13 <112951167+maralina13@users.noreply.github.com>
2025-02-03 16:04:56 +0300

    Update v1-create.sql

--- v1-create.sql
+++ v1-create.sql
@@ -1,3 +1,38 @@
 /**
- @author
+ @maralina13
 */
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    birth_date DATE,
+    country VARCHAR(50)
+);
+
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    author_id INTEGER REFERENCES authors(id) ON DELETE CASCADE,
+    genre_id INTEGER REFERENCES genres(id) ON DELETE CASCADE,
+    publication_year INTEGER,
+    isbn VARCHAR(200) UNIQUE
+);
+
+CREATE TABLE readers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    registration_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+    reader_id INTEGER REFERENCES readers(id) ON DELETE CASCADE,
+    order_date DATE DEFAULT CURRENT_DATE,
+    return_date DATE
+);
